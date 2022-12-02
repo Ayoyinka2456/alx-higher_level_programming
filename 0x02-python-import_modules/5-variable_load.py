02f4fcee69aa1e157f4e74cf982a2eef8da27718
Ayoyinka <kolawoleayoyinka.ka@gmail.com>
2022-12-01 20:07:11 -0800

    program that imports the variable a from the file variable_load_5.py and prints its value

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import variable_load_5
-    value_pull = variable_load_5.a
+    from variable_load_5 import a
+    value_pull = a
     print(value_pull)
